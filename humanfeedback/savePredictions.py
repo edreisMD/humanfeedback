dcfa3e91c0595537a9686120b302632261a53145
Eduardo Reis <edu.pontes@gmail.com>
2023-01-22 02:50:27 +0000

    edit wraper

--- humanfeedback/savePredictions.py
+++ humanfeedback/savePredictions.py
@@ -4,26 +4,6 @@ import pandas as pd
 from langchain import LLMChain
 import os
 
-# create a wraper for langchain LLMChain class to save the predictions in the database (for now a pandas dataframe in the working directory)
-class LLMChainWrapper(LLMChain):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # check if the file exists
-        if os.path.isfile('predictions.csv'):
-            # load the database
-            self.df = loadPredictions()
-        else:
-            # create a new database
-            self.df = pd.DataFrame(columns=['prediction'])
-            # save the database
-            self.df.to_csv('predictions.csv', index=False)
-
-    def predict(self, *args, **kwargs):
-        prediction = super().predict(*args, **kwargs)
-        self.df = self.df.append({'prediction': prediction}, ignore_index=True)
-        self.df.to_csv('predictions.csv', index=False)
-        return prediction 
-
 # functions to load the predictions from the database
 def loadPredictions():
     # load the database if it exists, otherwise create a new one
@@ -36,3 +16,31 @@ def loadPredictions():
         df = pd.DataFrame(columns=['prediction'])
     # return the database
     return df
+
+# create a wraper for langchain LLMChain class to save the predictions in the database (for now a pandas dataframe in the working directory)
+class Wrapper(object):
+    def __init__(self,wrapped_class):
+        self.wrapped_class = wrapped_class()
+
+    def __getattr__(self,attr):
+        orig_attr = self.wrapped_class.__getattribute__(attr)
+        if callable(orig_attr):
+            def hooked(*args, **kwargs):
+                self.pre()
+                result = orig_attr(*args, **kwargs)
+                # prevent wrapped_class from becoming unwrapped
+                if result == self.wrapped_class:
+                    return self
+                self.post()
+                return result
+            return hooked
+        else:
+            return orig_attr
+
+    def pre(self):
+        # load the database
+        self.df = loadPredictions()
+
+    def post(self):
+        # save the database
+        self.df.to_csv('predictions.csv',index=False)
