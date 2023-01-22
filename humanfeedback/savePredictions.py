9d4823fa55737c906dfb5bf8b3545e1e8bdb9682
Eduardo Reis <edu.pontes@gmail.com>
2023-01-22 03:05:04 +0000

    edit humanfeedback

--- humanfeedback/savePredictions.py
+++ humanfeedback/savePredictions.py
@@ -1,7 +1,5 @@
 # It is a pandas dataframe now but it will be replaced with a SQL database
 import pandas as pd
-
-from langchain import LLMChain
 import os
 
 # functions to load the predictions from the database
@@ -18,29 +16,20 @@ def loadPredictions():
     return df
 
 # create a wraper for langchain LLMChain class to save the predictions in the database (for now a pandas dataframe in the working directory)
-class Wrapper(object):
-    def __init__(self,wrapped_class):
-        self.wrapped_class = wrapped_class()
-
-    def __getattr__(self,attr):
-        orig_attr = self.wrapped_class.__getattribute__(attr)
-        if callable(orig_attr):
-            def hooked(*args, **kwargs):
-                self.pre()
-                result = orig_attr(*args, **kwargs)
-                # prevent wrapped_class from becoming unwrapped
-                if result == self.wrapped_class:
-                    return self
-                self.post()
-                return result
-            return hooked
-        else:
-            return orig_attr
-
-    def pre(self):
-        # load the database
-        self.df = loadPredictions()
-
-    def post(self):
-        # save the database
-        self.df.to_csv('predictions.csv',index=False)
+class Wraper(LLMChainClass):
+    def __init__(self, *args, **kwargs):
+        # call the init function of the LLMChainClass
+        super(Wraper, self).__init__(*args, **kwargs)
+        # load the predictions
+        self.predictions = loadPredictions()
+        # get the index of the last prediction
+        self.index = len(self.predictions)
+    def predict(self, *args, **kwargs):
+        # call the predict function of the LLMChainClass
+        prediction = super(Wraper, self).predict(*args, **kwargs)
+        # save the prediction in the database
+        self.predictions.loc[self.index] = prediction
+        # increase the index
+        self.index += 1
+        # return the prediction
+        return prediction
