d4d6282e2431b8c4d58bb565aeb25f3aa3484f3f
Eduardo Reis <edu.pontes@gmail.com>
2023-01-22 03:43:13 +0000

    edit

--- humanfeedback/savePredictions.py
+++ humanfeedback/savePredictions.py
@@ -28,18 +28,21 @@ def savePrediction(prediction):
 
 # create a wraper that receives a class called LLMChain or LLM and adda functionality when the function predict is called
 class CustomLLMChain(LLMChain):
-    # create a new constructor
-    def __init__(self, *args, **kwargs):
-        # call the original constructor
-        super().__init__(*args, **kwargs)
-        # save the original predict function
-        self.original_predict = self.predict
-
-    # create a new predict function
-    def predict(self, *args, **kwargs):
-        # call the original predict function
-        prediction = self.original_predict(*args, **kwargs)
-        # save the prediction to the database
+    # override predict function
+    def predict(self, **kwargs: Any) -> str:
+        """Format prompt with kwargs and pass to LLM.
+        Args:
+            **kwargs: Keys to pass to prompt template.
+        Returns:
+            Completion from LLM.
+        Example:
+            .. code-block:: python
+                completion = llm.predict(adjective="funny")
+        """
+        # add new functionality
+        # get the prediction
+        prediction = self(kwargs)[self.output_key]
+        # save the prediction
         savePrediction(prediction)
         # return the prediction
         return prediction
